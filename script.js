21bd1e804081ca6d0fe6b5b24f0e5f8f7453d615
collin cornelison <collincornelison@gmail.com>
2023-04-24 20:11:36 -0500

    message

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+//const { pickPlanet, addDestinationInfo } = require("./scriptHelper");
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+//const { myFetch } = require("./scriptHelper");
+
+//const { formSubmission } = require("./scriptHelper");
+
+window.addEventListener("load", function () {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let list = document.getElementById("faultyItems");
+    let pilot = document.querySelector("input[name=pilotName]");
+    let copilot = document.querySelector("input[name=copilotName]");
+    let fuelLevel = document.querySelector("input[name=fuelLevel");
+    let cargoLevel = document.querySelector("input[name=cargoMass]");
+
+    pilot = pilot.value;
+    copilot = copilot.value;
+    fuelLevel = fuelLevel.value;
+    cargoLevel = cargoLevel.value;
+
+    formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+  });
+  let listedPlanets;
+  // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+  let listedPlanetsResponse = myFetch();
+  listedPlanetsResponse
+    .then(function (result) {
+      listedPlanets = result;
+      console.log(listedPlanets);
+    })
+    .then(function () {
+      console.log(listedPlanets);
+      // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+   let pickedPlanet = pickPlanet(listedPlanets);
+
+   addDestinationInfo( document,
+    pickedPlanet.name,
+    pickedPlanet.diameter,
+    pickedPlanet.star,
+    pickedPlanet.distance,
+    pickedPlanet.moons,
+    pickedPlanet.image)
+    });
 });
